72f501951d578d058c189e8bd1db594f2ef98a0b
JonasHalver <44400498+JonasHalver@users.noreply.github.com>
2020-05-14 17:32:10 +0200

    Mirror / Prefab Tweaks

--- Assets/Scripts/Scripts/Mirror.cs
+++ Assets/Scripts/Scripts/Mirror.cs
@@ -4,19 +4,47 @@ using UnityEngine;
 
 public class Mirror : MonoBehaviour
 {
+    public static List<Mirror> mirrors = new List<Mirror>();
+    public int mirrorIndex;
     public Mirror linkedMirror;
     public MeshRenderer screen;
     public Camera playerCam;
     public Camera mirrorCam;
     private RenderTexture viewTexture;
     private MirrorCameraController c;
+    Vector3 playerToMirror;
+    public Transform handler;
 
     void Awake()
-    {
+    { 
+        if (!mirrors.Contains(this))
+        {
+            mirrors.Add(this);
+        }
+
+        screen = GetComponent<MeshRenderer>();
+
         if (!playerCam)
             playerCam = Camera.main;
 
         c = mirrorCam.GetComponent<MirrorCameraController>();
+    }
+
+    void Start()
+    {
+        for (int i = 0; i < mirrors.Count; i++)
+        {
+            if (mirrors[i] != this && mirrors[i].mirrorIndex == mirrorIndex)
+            {
+                linkedMirror = mirrors[i];
+                break;
+            }
+        }
+    }
+
+    void Update()
+    {
+        Render();
     }
 
     void CreateViewTexture()
@@ -40,6 +68,19 @@ public class Mirror : MonoBehaviour
         Render();
     }
 
+    public void Move()
+    {
+        mirrorCam.transform.localRotation = Quaternion.Euler(
+            new Vector3(playerCam.transform.rotation.eulerAngles.x,
+            playerCam.transform.rotation.eulerAngles.y,
+            playerCam.transform.rotation.eulerAngles.z));
+
+        playerToMirror = (playerCam.transform.position - linkedMirror.transform.position);
+        mirrorCam.transform.localPosition = playerToMirror;
+
+        handler.rotation = Quaternion.Euler(0, -linkedMirror.transform.localRotation.eulerAngles.y, 0);
+    }
+
     public void Render()
     {
         screen.enabled = false;
@@ -48,7 +89,8 @@ public class Mirror : MonoBehaviour
         mirrorCam.projectionMatrix = playerCam.projectionMatrix;
 
         //var m = transform.localToWorldMatrix * linkedMirror.transform.worldToLocalMatrix * playerCam.transform.localToWorldMatrix;
-        c.Move();
+        //c.Move();
+        Move();
         //mirrorCam.transform.SetPositionAndRotation(m.GetColumn(3), m.rotation);
 
         mirrorCam.Render();
